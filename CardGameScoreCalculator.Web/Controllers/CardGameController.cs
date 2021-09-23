1f747cb223c5523d5ba9335fef77c070d9d8312f
Jivitesh Trivedi <jivitesh13@hotmail.com>
2021-09-23 02:45:22 +0100

    Add project files.

---
+++ CardGameScoreCalculator.Web/Controllers/CardGameController.cs
@@ -0,0 +1,41 @@
+﻿//using CardGameScoreCalculator.Web.Models;
+using CardGameScoreCalculator.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGameScoreCalculator.Web.Controllers
+{
+    public class CardGameController : Controller
+    {
+        private readonly ILogger<CardGameController> _logger;
+
+        public CardGameController(ILogger<CardGameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CardGameModel());
+        }
+
+        [HttpPost]
+        public IActionResult Score(CardGameModel cardGameModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(nameof(this.Index));
+            }
+
+            ModelState.AddModelError("1", "Test error");
+            return View();
+        }       
+    }
+}
---
+++ CardGameScoreCalculator.Web/Models/CardGameModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGameScoreCalculator.Web.Models
+{
+    public class CardGameModel
+    {
+        [Required(ErrorMessage = "Cards Missing")]
+        [DisplayName("Enter your cards")]
+        public string Hand { get; set; }
+
+        public string Description =>
+            $"1.The list of cards must be given as a comma separated list.{Environment.NewLine}" +
+            $"2.Each card must use a two-character representation as follows:{Environment.NewLine}" +
+            $"  a) The first character represents the card’s value:" +
+            $"      2-9 for the card values 2 through 9{Environment.NewLine}" +
+            $"      T for 10{Environment.NewLine}" +
+            $"      J for a Jack{Environment.NewLine}" +
+            $"      Q for a Queen{Environment.NewLine}" +
+            $"      K for a King{Environment.NewLine}" +
+            $"      A for an Ace{Environment.NewLine}" +
+            $"  b) The second character represents the card’s suit:{Environment.NewLine}" +
+            $"      C for Clubs{Environment.NewLine}" +
+            $"      D for Diamonds{Environment.NewLine}" +
+            $"      H for Hearts{Environment.NewLine}" +
+            $"      S for Spades{Environment.NewLine}" +
+            $"  c) E.g. “AS” would be the Ace of Spades, '4C' would be the 4 of Clubs etc.{Environment.NewLine}" +
+            $"  d) A Joker is represented by two-character code 'JR'{Environment.NewLine}";
+
+    }
+}
